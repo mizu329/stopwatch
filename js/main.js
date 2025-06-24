261bbe05350f1d87da7e22cfd6bbc14d115e2254
mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2025-06-25 00:31:53 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+alert("これはストップウォッチです");
+
+var startButton = document.getElementsByClassName("startButton")[0];
+
+startButton.addEventListener("click", function () {
+  console.log("スタートボタンがクリックされました");
+});
