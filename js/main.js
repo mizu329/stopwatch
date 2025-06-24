87b92f079bbd6e29e55da2d83c9b145d55028320
mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2025-06-25 01:10:54 +0900

    clearIntervalを設定

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 alert("これはストップウォッチです");
 
 let displayElm = document.getElementsByClassName("display")[0];
+let timer = null;
+
 let startButton = document.getElementsByClassName("startButton")[0];
 
 startButton.addEventListener("click", function () {
@@ -8,9 +10,22 @@ startButton.addEventListener("click", function () {
 
   let seconds = 0;
 
-  setInterval(function () {
+  //   setInterval(function () {
+  //     seconds++;
+  //     displayElm.innerText = "経過時間: " + seconds + "秒";
+  //     console.log("経過時間: " + seconds + "秒");
+  //   }, 1000);
+
+  timer = setInterval(function () {
     seconds++;
     displayElm.innerText = "経過時間: " + seconds + "秒";
     console.log("経過時間: " + seconds + "秒");
   }, 1000);
+
+  let stopButton = document.getElementsByClassName("stopButton")[0];
+  stopButton.addEventListener("click", function () {
+    console.log("ストップボタンがクリックされました");
+    clearInterval(timer);
+    timer;
+  });
 });
