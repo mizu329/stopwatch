44ce4cdcd0596710b45160edc3d3be2741d3bd5b
mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2025-06-25 00:57:47 +0900

    ビジュアル実装

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 alert("これはストップウォッチです");
 
-var startButton = document.getElementsByClassName("startButton")[0];
+let displayElm = document.getElementsByClassName("display")[0];
+let startButton = document.getElementsByClassName("startButton")[0];
 
 startButton.addEventListener("click", function () {
   console.log("スタートボタンがクリックされました");
+
+  let seconds = 0;
+
+  setInterval(function () {
+    seconds++;
+    displayElm.innerText = "経過時間: " + seconds + "秒";
+    console.log("経過時間: " + seconds + "秒");
+  }, 1000);
 });
